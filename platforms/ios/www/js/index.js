1e5afaf6a695f432933020ee2f0c589c4101c2a5
Vlad Ivanovic <vladiim@me.com>
2014-01-09 22:01:32 +1100

    Added topcoat and edited to have textarea form

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,25 +1,10 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// http://phonegap.com/blog/2013/11/12/lxjs-workshop/
+
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        this.createStartPage();
     },
     // Bind Event Listeners
     //
@@ -33,7 +18,14 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        app.createHompage();
+        // app.receivedEvent('deviceready');
+    },
+    createStartPage: function() {
+        var body = $("body");
+        var html  = $("#navigation-bar").html() + $("#start-page").html();
+        var template = Handlebars.compile(html);
+        body.html(template({title: "Poop"}));
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
--- readme.md
+++ readme.md
@@ -20,6 +20,40 @@
 2. Create & manage note on iPhone
 3. Save note to server
 
+TODO:
+
+* Template with handlebars
+* notesAdapter.findById that gets an in memory note
+* Route function
+* Use require.js
+* To require html = require("text!views/note.html")
+* Angular
+* UI: top coat
+* Abstract device features
+		- notification
+		- storage
+		- online/offline
+		- mouse/touch
+		- etc
+* Event listeners: ftlabs/fastclick
+* Set up hardware accelerator
+* When setting up pages have:
+		- right (right of viewport)
+				* css: .right { left: 100%; } # slow, not hardware accelerated
+				* .page { transform: translate3d(0, 0, 0); }
+				* .right { transform: translate3d(-100%, 0, 0); } # fast!
+		- centre
+		- left (left of VP)
+		- To change views, change the class name
+* Add css html { -webkit-touch-callout: none; -webkit-tap-highligh-color: rgba(0,0,0) }
+* Compilers: coffeescript, scss, guard
+
+***
+
+# Compiling
+
+* Test on ios `$ phonegap run ios`
+
 ***
 
 # Dependencies
