48cde4c6dbeddf27345ea2f3d274cff9de3c4bea
Vlad Ivanovic <vladiim@me.com>
2014-01-12 09:35:45 +1100

    Jasmine test runner working with requirejs

--- www/spec/helper.js
+++ www/spec/helper.js
@@ -1,82 +1,50 @@
 require.config({
-  baseUrl: './',
-
+  baseUrl: "./",
+  urlArgs: 'cb=' + Math.random(),
   paths: {
-    app: './js/app',
-    helpers: './js/helpers',
-    spec: './spec',
-    jquery: './js/helpers/jquery'
+    jquery: 'js/helpers/jquery',
+    jasmine: 'spec/lib/jasmine-1.2.0/jasmine',
+    "jasmine-html": 'spec/lib/jasmine-1.2.0/jasmine-html',
+    spec: 'spec'
+  },
+
+  shim: {
+    jasmine: {
+      exports: 'jasmine'
+    },
+    'jasmine-html': {
+      deps: ['jasmine'],
+      exports: 'jasmine'
+    }
   }
 });
 
-afterEach(function() {
-    document.getElementById('stage').innerHTML = '';
-});
-
-var helper = {
-    trigger: function(obj, name) {
-        var e = document.createEvent('Event');
-        e.initEvent(name, true, true);
-        obj.dispatchEvent(e);
-    },
-    getComputedStyle: function(querySelector, property) {
-        var element = document.querySelector(querySelector);
-        return window.getComputedStyle(element).getPropertyValue(property);
-    }
-};
-
-var jasmineEnv = jasmine.getEnv();
-jasmineEnv.updateInterval = 1000;
-
-var htmlReporter = new jasmine.HtmlReporter();
-
-jasmineEnv.addReporter(htmlReporter);
-
-jasmineEnv.specFilter = function(spec) {
-    return htmlReporter.specFilter(spec);
-};
-
-// var currentWindowOnload = window.onload;
-
-// window.onload = function() {
-//     if (currentWindowOnload) {
-//         currentWindowOnload();
-//     }
-//     execJasmine();
-// };
-
-// require(["spec/router_spec.js", "app/router", "helpers/jquery"], function(router_spec, router, $) {
-//     $(document).ready(function() {
-//         console.log('doc ready');
-//     });
-//     jasmineEnv.execute();
-
-//     // router_spec.runSpecs(router);
-//     describe('test', function() {
-//       console.log('describe called' + it('test', function() {return 'poo';}));
-//         it('test', function() {
-//             console.log('it called');
-//             expect(true).toEqual(false);
-//         });
-//     });
-// });
-
-jasmineEnv.execute()
-
-runRouterSpecs = function() {
+runSpecs = function() {
   return define(function(require) {
-    var router      = require('app/router'),
-        router_spec = require('spec/router_spec');
+    var $       = require('jquery'),
+        jasmine = require('jasmine-html'),
+        jasmineEnv = jasmine.getEnv();
 
-    // jasmineEnv.execute()
+    jasmineEnv.updateInterval = 1000;
 
-    describe('test', function() {
-      it('tests shit out', function() {
-        expect(true).toEqual(true);
+    var htmlReporter = new jasmine.HtmlReporter();
+
+    jasmineEnv.addReporter(htmlReporter);
+
+    jasmine.specFilter = function(spec) {
+      return htmlReporter.specFilter(spec);
+    };
+
+    var specs = [];
+
+    specs.push("spec/routerSpec");
+
+    $(function() {
+      require(specs, function() {
+        jasmineEnv.execute()
       });
     });
   });
 };
 
-runRouterSpecs();
-
+runSpecs();
---
+++ www/spec/routerSpec.js
@@ -0,0 +1,5 @@
+describe('testing', function() {
+  it('blah', function() {
+    expect(1).toEqual(1);
+  });
+});
